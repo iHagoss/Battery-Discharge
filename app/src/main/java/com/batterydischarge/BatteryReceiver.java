a2365e833b7d040098a15a57314a0753422a4bd1
iHagoss <hagos@live.com>
2025-06-30 20:04:33 +1000

    BatteryReceiver.java

---
+++ app/src/main/java/com/batterydischarge/BatteryReceiver.java
@@ -0,0 +1,26 @@
+```java
+package com.batterydischarge;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BatteryReceiver extends BroadcastReceiver {
+    
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        
+        if (Intent.ACTION_POWER_DISCONNECTED.equals(action)) {
+            // Start monitoring when charger disconnected
+            Intent serviceIntent = new Intent(context, BatteryService.class);
+            context.startForegroundService(serviceIntent);
+            
+        } else if (Intent.ACTION_POWER_CONNECTED.equals(action)) {
+            // Stop discharge monitoring when charger connected
+            NotificationHelper helper = new NotificationHelper(context);
+            helper.hideNotification();
+        }
+    }
+}
+```
